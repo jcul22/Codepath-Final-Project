e0762e4d7f16f7e44d4142086802484d104c2e7f
ZienHambrick <77498808+ZienHambrick@users.noreply.github.com>
2022-04-18 21:06:13 -0500

    Add files via upload

---
+++ JSAnimalServices/PostCell.swift
@@ -0,0 +1,32 @@
+//
+//  PostCell.swift
+//  
+//
+//  Created by Antonio hambrick on 4/18/22.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
